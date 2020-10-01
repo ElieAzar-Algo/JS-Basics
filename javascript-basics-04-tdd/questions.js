5e2658d3a24a7b853cb4ec7619d0f89134bccca1
ElieAzar-Algo <elie.azar3891.ea@gmail.com>
2020-10-01 16:54:40 +0300

    basics js 4 done

--- javascript-basics-04-tdd/questions.js
+++ javascript-basics-04-tdd/questions.js
@@ -1,75 +1,102 @@
 
 const stringSize = (text) => {
+    return text.length;
 
 }
 const replaceCharacterE = (text) => {
+   return  text.replace("e"," ");
+
 
 }
 const concatString = (text1, text2) => {
+    return text1.concat(text2);
 
 }
 const showChar5 = (text) => {
 
+   return text.charAt(4);
 }
 const showChar9 = (text) => {
+    return text.substring(0,9);
 
 }
 const toCapitals = (text) => {
+  return  text.toUpperCase();
 
 }
 const toLowerCase = (text) => {
+    return text.toLowerCase();
 
 }
 const removeSpaces = (text) => {
+    return text.trim();
 
 }
 const IsString = (text) => {
+    return (typeof text==="string");
 
 }
 
 const getExtension = (text) => {
-
+    return text.split('.').pop();
 }
 const countSpaces = (text) => {
+    return (text.split(" ").length - 1);
 
 }
 const InverseString = (text) => {
-
-}
+    
+        return text.split("").reverse().join("");
+    }
 
 const power = (x, y) => {
+    return Math.pow(x,y);
+
 
 }
 const absoluteValue = (num) => {
+    return Math.abs(num);
 
 }
 const absoluteValueArray = (array) => {
-
+    for(var i=0 ; i<array.length; i++){
+    if (array[i] < 0){
+        array[i]*=-1;}
+    return array[i];
+    }
 }
 const circleSurface = (radius) => {
-
+    var s=3.149999*(radius*radius);
+    s=parseInt(s)+1;
+ 
+    return s;
+    
 }
 const hypothenuse = (ab, ac) => {
+    return Math.hypot(ab, ac);
 
 }
 const BMI = (weight, height) => {
 
+    return parseFloat((weight /( height * height)).toFixed(2));
 }
 
 const createLanguagesArray = () => {
+    return ["Html", "CSS", "Java", "PHP"];
 
 }
 
 const createNumbersArray = () => {
-
+   
 }
 
 const replaceElement = (languages) => {
-
+    
 }
 
 const addElement = (languages) => {
-
+    
+   
 }
 
 const addNumberElement = (numbers) => {
